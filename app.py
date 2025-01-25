1f9e6d7074666739d1a54d2227345704b69412b8
Pragyan Coder <107480064+PragyanCoder@users.noreply.github.com>
2025-01-25 21:47:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
 import os
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def welcome():
-    # Render the welcome page with animated "Team SPY" text
-    return render_template("welcome.html")
+@app.route('/')
+def home():
+    return """
+    <center>
+        <img src="/static/pragyan.jpg" style="border-radius: 2px;"/>/>
+    </center>
+    <style>
+        body {
+            background: antiquewhite;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            height: 100vh;
+            margin: 0;
+        }
+        footer {
+            text-align: center;
+            padding: 10px;
+            background: antiquewhite;
+            font-size: 1.2em;
+        }
+    </style>
+    <footer>
+        Made with 💕 by @Pragyan
+    </footer>
+    """
 
 if __name__ == "__main__":
-    # Default to port 5000 if PORT is not set in the environment
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=port)
