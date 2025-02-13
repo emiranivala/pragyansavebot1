842a794d212d92198f31b9e27f40b8b5bba470f6
Pragyan Coder <107480064+PragyanCoder@users.noreply.github.com>
2025-02-13 14:14:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,34 @@
+
 # devgagan
 # Note if you are trying to deploy on vps then directly fill values in ("")
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", "25121213"))
-API_HASH = getenv("API_HASH", "b734dcc45da130a8156e2be836594706")
-BOT_TOKEN = getenv("BOT_TOKEN", "7842255168:AAEiEtOdBj4SugCNih1_CIigHO3x6NRV6qM")
+# VPS --- FILL COOKIES 🍪 in """ ... """ 
+
+INST_COOKIES = """
+# wtite up here insta cookies
+"""
+
+YTUB_COOKIES = """
+# write here yt cookies
+"""
+
+API_ID = int(getenv("API_ID", "20348897"))
+API_HASH = getenv("API_HASH", "ecfb9a700b8398caf58c53f36ccf5b06")
+BOT_TOKEN = getenv("BOT_TOKEN","8037339627:AAG92sWh1gQwDGtdaJJ_X916otdY5eCd9n4")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "5464921200").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://PihuMusic:PihuMusic@cluster0.w3eiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-LOG_GROUP = getenv("LOG_GROUP", "-1002380299642")
+LOG_GROUP = getenv("LOG_GROUP", "-1002283846449")
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1001573605549"))
-#CHANNEL_USERNAME="@VRindavanNeeko16008"
-FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "1000000000000000000000000000000000000000000000000"))
-PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "5001000000000000000000000000000000000000000000000000"))
+FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "500"))
+PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
 AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
 STRING = getenv("STRING", None)
-YT_COOKIES = getenv("YT_COOKIES", None)
-INSTA_COOKIES = getenv("INSTA_COOKIES", None)
 
+# YouTube Cookies Configuration (ensure to replace this with valid cookies)
+#YT_COOKIES = getenv("YT_COOKIES", "GPS=1; YSC=VW83QNuF-rE; __Secure-ROLLOUT_TOKEN=CPfXrsuG1-m97gEQ-L_JhfWBiwMY-L_JhfWBiwM%3D; VISITOR_INFO1_LIVE=myhtpsWPqDg; VISITOR_PRIVACY_METADATA=CgJJThIEGgAgUg%3D%3D; PREF>
 
+# Instagram Cookies (if required)
+#INSTA_COOKIES = getenv("INSTA_COOKIES", None)
